b0ebd2d5cd58daa3d8239f4a3174736c38a2330e
Marc Mills <mdmills1990@gmail.com>
2018-08-27 22:28:34 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |seasons, holidays|
     holidays.map do |holiday, supplies|
       if supplies.include?("BBQ")
-        return holiday
+      holiday
       end
     end
   end.flatten.compact
