3de77a765242e44ead9233718cf2230e01b2350d
Marc Mills <mdmills1990@gmail.com>
2018-08-27 20:00:53 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,8 +20,7 @@ end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |seasons, holidays|
-    seasons.each do |holidays, supplies|
-    end
+    binding.pry
   end
 end
 
