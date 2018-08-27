5d3d88361e60caa2673ec8a670d7234ab2bc27d3
Marc Mills <mdmills1990@gmail.com>
2018-08-27 22:25:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,9 +60,9 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.each do |seasons, holidays|
-    holidays.each do |holiday, supplies|
-      if supplies == "BBQ"
+  holiday_hash.collect do |seasons, holidays|
+    holidays.collect do |holiday, supplies|
+      if supplies.include?("BBQ")
         return holiday
       end
     end
