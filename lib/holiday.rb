151864058fdbb7370d607a8b32cdabbe4b331a24
Marc Mills <mdmills1990@gmail.com>
2018-08-27 19:15:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,7 +17,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
        :memorial_day => ["BBQ"]
      }
    }
-return holiday_hash[:summer][:fourth_of_july][1]
+holiday_hash[:summer][:fourth_of_july][1] = "Watermelon"
+return holiday_hash
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
