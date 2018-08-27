7b5b333221040c7236eed419d2b096f4fe50f0d4
Marc Mills <mdmills1990@gmail.com>
2018-08-27 20:44:16 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,7 +43,7 @@ holiday_hash[season] = {holiday_name => supply_array}
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-holiday_hash[:winter].value
+holiday_hash[:winter].values
 end
 
 def all_supplies_in_holidays(holiday_hash)
